993d5a579f356cf90d4b5336548b685b8552abac
tsafay <287672785@qq.com>
2020-05-12 21:31:17 +0800

    book learning program of automation python

--- book_learning/automation_py/ch17_操作图像/draw_text.py
+++ book_learning/automation_py/ch17_操作图像/draw_text.py
@@ -7,8 +7,9 @@ from PIL import Image,ImageDraw,ImageFont
 img = Image.new('RGBA',(200,200),'white')
 draw = ImageDraw.Draw(img)
 draw.text((20,150),'hello',fill='red')
-fontsFolder = 'FONT_FOLDER'
-arialFont = ImageFont.truetype(os.path.join(fontsFolder,'arial.ttf'),32)
-draw.text((100,150),'world',fill='gray',font=arialFont)
+#fontsFolder = 'FONT_FOLDER'
+#arialFont = ImageFont.truetype(os.path.join('FONT_FOLDER','arial.ttf'),40)
+arialFont = ImageFont.truetype('ariali.ttf',40)
+draw.text((100,150),'world',fill='blue',font=arialFont)
 img.save('text.png')
 
